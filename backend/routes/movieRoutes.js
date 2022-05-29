3415c0c0385025e82b5115fbe846ec16487f0971
Gabriel <60167780+Gabmelom@users.noreply.github.com>
2022-05-29 01:41:07 -0400

    feat(api): movies route to query db collection

--- backend/routes/movieRoutes.js
+++ backend/routes/movieRoutes.js
@@ -2,15 +2,53 @@ const express = require('express');
 const router = express.Router();
 const Movie = require('../models/movieModel');
 
-// TODO Movie page should show review count, average rating, and review comments
+// TODO Movie page should show review count, average rating, and review comments <- needs to be queried apart
 
-router.get('/', async (_,res) => {
+// router.post('/', authorize,createMovie, updatePeople, sendNotifications);
+// router.param("id",findMovie);
+// router.get('/:id', inWatchlist, loadMovie);
+// router.get('/:id/reviews', paginationParser, loadReviews);
+// router.post('/:id/reviews', addReview);
+
+router.get('/', async (req,res) => {
+    const MAX_RESULTS = 30;
+    let query = Movie.find().select('title poster');
+    
+    // Parse pagination params
+    req.query.page = Number(req.query.page);
+    if (!req.query.page || req.query.page < 1) 
+        req.query.page = 1;
+    query = query.limit(MAX_RESULTS).skip((req.query.page-1) * MAX_RESULTS);
+
+    // Parse movie query params
+    req.query.year = Number(req.query.year);
+    if (req.query.title) query = query.where('title').regex(new RegExp(`.*${req.query.title}.*`, 'i'));
+    if (req.query.genre) query = query.where('genre').regex(new RegExp(`.*${req.query.genre}.*`, 'i'));
+    if (req.query.year)  query = query.where('year').gte(req.query.year);
+    
     try {
-        const movies = await Movie.find();
+        const movies = await query.exec();
         res.json(movies);
     } catch (error) {
-        res.status(500).send('Database error');
+        res.status(500).send('Server error');
     }
+});
+
+router.param('id', async (req, res, next, id) => {
+    try {
+        req.movie = await Movie.findById(id);
+        if (req.movie === null)
+            return res.status(404).send('Not Found');
+    } catch (error) {
+        if (error.name === 'CastError') 
+            return res.status(400).send('Bad request')
+        return res.status(500).send('Server error');
+    }
+    next();
+});
+
+router.get('/:id', async (req,res) => {
+    res.json(req.movie);
 });
 
 router.post('/', async (_,res) => {
---
+++ backend/routes/personRoute.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const Person = require('../models/personModel');
+
+// router.get('/', paginationParser,queryParser,searchDatabase,sendPeople);
+// router.post('/', authorize,createPerson, updateMovies, sendNotifications);
+// router.param("id",findPerson);
+// router.get('/:id', isFollower, loadPerson);
