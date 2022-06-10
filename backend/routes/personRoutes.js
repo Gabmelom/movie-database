7f89c7fd9213d511483b54fc7d878fe139a926c0
Gabriel <60167780+Gabmelom@users.noreply.github.com>
2022-06-10 12:31:48 -0400

    feat(frontend): very simple home page with forms to query movies and persons

--- backend/routes/personRoutes.js
+++ backend/routes/personRoutes.js
@@ -2,10 +2,6 @@ const asyncHandler = require('express-async-handler');
 const router = require('express').Router();
 const Person = require('../models/personModel');
 
-// router.get('/', paginationParser,queryParser,searchDatabase,sendPeople);
-// router.post('/', authorize,createPerson, updateMovies, sendNotifications);
-// router.param("id",findPerson);
-// router.get('/:id', isFollower, loadPerson);
 
 router.get('/', asyncHandler(async (req, res) => {
     const MAX_RESULTS = 30;
