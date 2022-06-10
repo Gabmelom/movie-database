309c2769ed8f074ec718e84b81760374b8819a29
Gabriel <60167780+Gabmelom@users.noreply.github.com>
2022-06-10 11:53:32 -0400

    feat(api): route to query persons db collection

--- backend/routes/personRoutes.js
+++ backend/routes/personRoutes.js
@@ -8,7 +8,20 @@ const Person = require('../models/personModel');
 // router.get('/:id', isFollower, loadPerson);
 
 router.get('/', asyncHandler(async (req, res) => {
+    const MAX_RESULTS = 30;
+    let query = Person.find().select('name');
     
+    // Parse pagination parameters
+    req.query.page = Number(req.query.page);
+    if (!req.query.page || req.query.page < 1) 
+        req.query.page = 1;
+    query = query.limit(MAX_RESULTS).skip((req.query.page-1) * MAX_RESULTS);
+
+    // Parse person query parameters
+    if (req.query.name) query = query.where('name').regex(new RegExp(`.*${req.query.name}.*`, 'i'));
+    
+    const persons = await query.exec();
+    res.status(200).json(persons);
 }));
 
 module.exports = router;
